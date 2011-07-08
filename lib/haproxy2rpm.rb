d5cde60b2f38272254dc1e390eb5eacc024046fb
Patrick Huesler <patrick.huesler@gmail.com>
2011-07-08 22:12:42 +0200

    add daemonize option

--- lib/haproxy2rpm.rb
+++ lib/haproxy2rpm.rb
@@ -8,7 +8,21 @@ require "haproxy2rpm/file_parser"
 require "haproxy2rpm/line_parser"
 
 module Haproxy2Rpm
-  def self.run(log_file)
+  def self.run(log_file, options)
+    if(options[:daemonize])
+      puts 'daemonizing'
+      run_daemonized(log_file, options)
+    else
+      default_run(log_file, options)
+    end
+  end
+
+  def self.stop
+    puts 'stopping new relic agent'
+    NewRelic::Agent.shutdown
+  end
+  
+  def self.default_run(log_file,options)
     NewRelic::Agent.manual_start
     stats_engine = NewRelic::Agent.agent.stats_engine
     EventMachine.run do
@@ -18,9 +32,26 @@ module Haproxy2Rpm
       end
     end
   end
-
-  def self.stop
-    puts 'stopping new relic agent'
-    NewRelic::Agent.shutdown
+  
+  def self.run_daemonized(log_file, options)
+      Signal.trap('HUP') {}
+      
+      pid = fork do
+        begin
+          default_run(log_file, options)
+        rescue => e
+          puts e.message
+          puts e.backtrace.join("\n")
+          abort "There was a fatal system error while starting haproxy2rpm"
+        end
+      end
+      
+      if options[:pid]
+        File.open(options[:pid], 'w') { |f| f.write pid }
+      end
+      
+      ::Process.detach pid
+      
+      exit
   end
 end
