83dca1dc12950d5f3fbd2d9b8650f76d26a57656
Patrick Huesler <patrick.huesler@gmail.com>
2011-07-11 19:26:29 +0200

    BUGFIX: only send an error message, if there is an actual error

--- lib/haproxy2rpm/rpm.rb
+++ lib/haproxy2rpm/rpm.rb
@@ -11,12 +11,16 @@ module Haproxy2Rpm
 
     def process_and_send(line)
       request = LineParser.new(line)
-      NewRelic::Agent.record_transaction(
-        request.tr / 1000.0,
-        'metric' => "Controller#{request.http_path}",
-        'is_error' => request.is_error?,
-        'error_message' => "#{request.uri} : Status code #{request.status_code}"
-      )
+      params = {
+        'metric' => "Controller#{request.http_path}"
+      }
+
+      if request.is_error?
+        params['is_error'] = true
+        params['error_message'] = "#{request.uri} : Status code #{request.status_code}"
+      end
+
+      NewRelic::Agent.record_transaction(request.tr / 1000.0, params)
       @qt_stat.record_data_point(request.tw / 1000.0)
     end
   end
